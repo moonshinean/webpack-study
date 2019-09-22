5bc38fe63912b752fca26b5490fda95c2f7d1b55
moonshine <710159940@qq.com>
2019-09-22 21:50:32 +0800

    webpack study：    样式文件配置

--- src/main.js
+++ src/main.js
@@ -6,8 +6,26 @@
 // 由于ES6的代码 ， 太高级了 ，浏览器解析不了， 所以，这一执行会报错。
 import $ from 'jquery'
 
+// 使用 import 语法，导入 css 样式表。
+import './css/index.css'
+
+import './css/index.less'
+import './css/index.sass'
+// 注意： webpack，默认只能打包处理Js 类型的文件，无法处理 其他非js类型的文件；
+// 如果要处理 非JS类型的文件，我们需要手动安装一些 合适的 第三方 loader 加载器；
+// 1、如果箱套打包处理css文件，需要手动安装 npm i style-loader css-loader -D
+// 2、打开 webpack.config.js 这个配置文件，在里面，新增一个配置节点，叫做 module，它是一个对象
+// 在这个module 对象身上，有个rules属性， 这个rules 属性是个数组，这个数组中，存放了，所有第三方文件的匹配和处理规则;
+
+
+// 注意：webpack 处理第三方文件类型的过程
+// 1、发现这个 需要处理的文件是不是JS文件， 然后就去 配置文件中，查找有没有对应的第三方 loder 规则
+// 2、如果能找到对应的规则，就会调用对应的 loader 处理，这种文件类型；
+// 3、在调用loader 的时候，是从后往前调用的：
+// 4、当最后一个loader 调用完毕，会把处理的结果，直接交给webpack 进行打包合并。 最终输出到 bundle.js中去
+
 $(function(){
-    $('li:odd').css('background', 'red');
+    $('li:odd').css('background', 'green');
     $('li:even').css('background', function(){
         return '#' +'D97634'
     });
@@ -20,3 +38,14 @@ $(function(){
 // 2、webpack 能够处理JS 的兼容问题， 把高级的、浏览器不能识别的语法，转为 低级的、 浏览器能识别的语法
 
 // 钢刚才运行的命令是： webpack  要打包的文件地址   打包好的文件地址
+
+
+// 使用 webpack-dev-server 这个工具，来实现自动打编译的功能
+// 1、运行 npm i webpack-dev-server -D 把这个工具安装到项目本地开发依赖
+// 2、安装完毕后，这个 工具的用法，和webpack 命令的用法，完全一样
+// 3、由于是在本地安装的 webpack-dev-server，所以无法当做脚本命令在终端中直接运行；(只有安装到全局-g的工具，才能在终端中正常执行)
+// 4、注意： webpack-dev-server 这个工具, 如果想要正常运行， 需求，在本地项目中，需要在安装webpack
+// 5、webpack-dev-server 帮我们 打包生成的 bundle.js 文件，并没有存放在 实际的 物理磁盘；而是，直接托管到了 电脑内存中 ，
+// 所以，我们在项目根目录中,根本找不到这个打包好的 bundle.js
+// 6、我们可以认为，webpack-dev-server 把打包好的文件，以一种虚拟的形式，托管到了咱们的项目的根目录中，虽然我们看不见它，但是，可以认为  和dist
+// src node_modules 平级，有一个看不见的文件, 叫做 bundle.js
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,56 @@
-const path = require('path')
+const path = require('path');
+
+// 启用热更新的 第2步
+// const webpack = require('webpack')(webpack 4版本的不需要直接使用 host：true 就可以了)
+
+// 导入在内存中生成的HTML 页面的插件
+// 只要是插件，都一定要放到 plugins: 中去
+
+// 这个插件的两个作用：
+// 1、自动在内存中根据指定的页面生成一个内存的页面，
+// 2、自动，把打包好的 bundle.js 追加到页面中。
+const htmlWebpackPlugin = require('html-webpack-plugin')
 // 这个配置文件，起始就是一个JS文件，通过 Node 中的模块操作，向外暴露一个配置对象
 module.exports = {
       //  在配置文件中， 需要手动去指定 出口和入口
-      entry:  path.join(__dirname, './src/main.js'),// 入口，表示要打包那个文件。 
+      entry:  path.join(__dirname, './src/main.js'),// 入口，表示要打包那个文件。
       output: {
           path: path.join(__dirname, './dist'), //制动 打包好的文件，输出到那个目录中去
           filename: 'bundle.js' // 这个是指定输出的文件名称
       },
-      mode: 'development' // 设置mode
+    //   mode: 'development' // 设置mode
+    devServer:{   //这是配置 dev-serve 命令参数的第二中形式，相对来说，这种方式麻烦一些
+        //    " --open --port 3000 --contentBase src --hot"
+       open: true, // 自动打开浏览器
+       port: 3000, //设置启动时候运行的端口
+       contentBase: 'src', //指定托管的根目录
+       hot: true  // 启动热更新
+    },
+    plugins: [  // 配置插件节点
+        // new webpack.HotModuleReplacementPlugin(),   new 一个热更新的 模板对象 这是启用热更新的第三步 （适用于 webpack 3版本）
+        new htmlWebpackPlugin({  // 穿件一个 在内存中 生成HTML 页面的插件
+            template: path.join(__dirname, './src/index.html'), // 指定模板页面，将会根据指定的页面路径去生成内存中的页面
+            filename: 'index.html'  // 指定生成页面的名称
+        })
+    ],
+    module: { // 这个节点，用于配置 所有第三方模块 加载器
+       rules: [  // 所有第三方模块 的 匹配规则
+            {test: /\.css$/, use: ['style-loader', 'css-loader']}, // 配置处理 .css文件的第三方loader,
+            // **注意： 此处 style-loader和css-loader顺序不能反(从后面直接调用到前面)
+            {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
+            // 这是 配置less 第三方加载器 规则
+            {test: /\.sass$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+             // 这是 配置sass 第三方加载器 规则
+             {test: /\.(jpg|png|gif|bmp|jpeg)$/, use: ['url-loader?limit=7631&name=[hash:8]-[name].[ext]']},
+            //  图片配置 的loader
+            // limit 给定的值，是图片的大小，单位是byte， 如果我们引用的 图片，大于或等于给定的limit值，
+            // 则不会转为base64格式的字符串，如果 图片小于给定的 limit 值，则会被转为 base64 的字符串
+            // name=[name].[ext]  表示图片原来的名字是什么名字，打包后名字不变，图片格式不变（此处为固定写法）
+            // [hash:8] 表示在图片名字前面拼接8位任意的哈希值。
+            {test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader'},  // 处理字体文件
+            // {test: /\.(ttf|eot|svg|woff|woff2)$/}
+       ]
+    }
 }
 
 // 当我们在控制台， 直接输入，webpack 命令执行的时候， webpack 做了一下几步
